79f61765e4ad39bc965a7e3909fe82dd5e7adc6e
Gael Rodríguez <gaelrodriguezl474@gmail.com>
2023-07-09 21:29:17 -0600

    The form for edit records has been modified.

--- src/components/Paciente/EditarPacienteComponent.js
+++ src/components/Paciente/EditarPacienteComponent.js
@@ -1,11 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import TextField from "@mui/material/TextField";
 import axios from "axios";
-
-import LinearProgress from "@mui/material/LinearProgress";
-import Box from "@mui/material/Box";
-
 import DeleteIcon from "@mui/icons-material/Delete";
 import SaveIcon from "@mui/icons-material/Save";
 import {
@@ -23,19 +18,15 @@ import DialogTitle from "@mui/material/DialogTitle";
 import Slide from "@mui/material/Slide";
 
 const Transition = React.forwardRef(function Transition(props, ref) {
-  //Efecto de transición
   return <Slide direction="up" ref={ref} {...props} />;
 });
 
 function EditarPacienteComponent() {
   const location = useLocation();
 
-  //Menu de opciones
   const actions = [
     { icon: <DeleteIcon />, name: "Eliminar", key: "eliminar" },
     { icon: <SaveIcon />, name: "Guardar", key: "guardar" },
-    //{ icon: <PrintIcon />, name: 'Print' },
-    //{ icon: <ShareIcon />, name: 'Share' },
   ];
 
   const handleFunction = (e, key) => {
@@ -49,79 +40,69 @@ function EditarPacienteComponent() {
   };
 
   const navigate = useNavigate();
-  //
-
-  //Animacion de carga
   const [loading, setLoading] = useState(false);
 
-  //Creación del objeto
   const [paciente, setPaciente] = useState({
-    id: 0, //el valor es int
-    nombre: "", //el valor es string
+    id: 0,
+    nombre: "",
     edad: 0,
     nss: "",
     domicilio: "",
   });
 
   const inputChange = (event) => {
-    //Permite la manipulación de datos obtenidos
     setPaciente({
       ...paciente,
       [event.target.name]: event.target.value,
     });
   };
 
-  //Creación del objeto -END
-
-  //Consulta de un paciente
+  //Consulta - START
   const fnObtenerDatos = async () => {
-    setLoading(true); //Animacion de carga true
+    setLoading(true);
     await axios
       .get("http://127.0.0.1:8000/api/paciente", {
         params: {
-          id: location.state.id, //Se utiliza el parametro dado en PacientesComponent
+          id: location.state.id,
         },
       })
       .then((response) => {
         console.log(response.data);
-        setPaciente(response.data); //Agregar los datos para su visualizacion
-        setLoading(false); //animacion de carga false
+        setPaciente(response.data);
+        setLoading(false);
       })
       .catch((error) => { });
   };
 
   useEffect(() => {
     console.log("Render");
-    //Si el id tiene datos realiza la consulta, sino, no realizar la consulta y se crea
     if (location.state.id !== 0) {
-      fnObtenerDatos(); //para que se vea en consola la funcion
+      fnObtenerDatos();
     }
   }, []);
-  //Consulta de un paciente -END
+  //Consulta -END
 
-  //Actualización de un paciente
+  //Actualizar - START
   const fnActualizarDatos = async () => {
     console.log("Nombre: " + paciente.nombre);
     console.log("Edad: " + paciente.edad);
     console.log("NSS: " + paciente.nss);
     console.log("Domicilio: " + paciente.domicilio);
 
-    setLoading(true); //Animacion de carga true
+    setLoading(true);
     await axios
       .post("http://127.0.0.1:8000/api/paciente/crear", paciente)
       .then((response) => {
         console.log(response.data);
-        setPaciente(response.data); //Agregar los datos para su visualizacion
-        setLoading(false); //animacion de carga false
+        setPaciente(response.data);
+        setLoading(false);
 
         navigate("/pacientes");
       })
       .catch((error) => { });
   };
+  //Actualizar -END
 
-  //Actualización de un paciente -END
-
-  //Mensaje de confirmacion para eliminar paciente
   const [open, setOpen] = React.useState(false);
 
   const handleClickOpen = () => {
@@ -132,31 +113,28 @@ function EditarPacienteComponent() {
     setOpen(false);
   };
 
-
-  //Mensaje de confirmacion para eliminar paciente -END
-
-  //Eliminar un paciente
+  //Eliminar - START
   const fnEliminarDatos = async () => {
     console.log("ID: " + paciente.id);
 
-    setLoading(true); //Animacion de carga true
+    setLoading(true);
     await axios
       .post("http://127.0.0.1:8000/api/paciente/borrar", paciente)
       .then((response) => {
         console.log(response.data);
-        setPaciente(response.data); //Agregar los datos para su visualizacion
-        setLoading(false); //animacion de carga false
+        setPaciente(response.data);
+        setLoading(false);
 
         navigate("/pacientes");
       })
       .catch((error) => { });
-    };
-    
+  };
+  //Eliminar - END
 
   return (
     <>
       {
-        //Mensaje de confirmación para eliminar
+        //Modal de confirmación para eliminar
       }
       <Dialog
         open={open}
@@ -199,49 +177,61 @@ function EditarPacienteComponent() {
       </SpeedDial>
 
       <form>
-        <h2>ID: {location.state.id}</h2>
-        <div className="mb-2">
-          <label>Nombre</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Ingresa Nombre"
-            name='nombre'
-            value={paciente.nombre} onChange={(e) => inputChange(e)}
-          />
-        </div>
+        <div className="form-container">
+          <div className="form-inner">
+            <h3>Editar Paciente</h3>
+            <div className="mb-3">
+              <label htmlFor="nombre" className="form-label">Nombre</label>
+              <input
+                type="text"
+                className="form-control"
+                id="nombre"
+                placeholder="Ingresa Nombre"
+                name="nombre"
+                value={paciente.nombre}
+                onChange={(e) => inputChange(e)}
+              />
+            </div>
 
-        <div className="mb-2">
-          <label>Edad</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Ingresa Edad"
-            name='edad'
-            value={paciente.edad} onChange={(e) => inputChange(e)}
-          />
-        </div>
+            <div className="mb-3">
+              <label htmlFor="edad" className="form-label">Edad</label>
+              <input
+                type="text"
+                className="form-control"
+                id="edad"
+                placeholder="Ingresa Edad"
+                name="edad"
+                value={paciente.edad}
+                onChange={(e) => inputChange(e)}
+              />
+            </div>
 
-        <div className="mb-2">
-          <label>NSS</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Ingresa NSS"
-            name='nss'
-            value={paciente.nss} onChange={(e) => inputChange(e)}
-          />
-        </div>
+            <div className="mb-3">
+              <label htmlFor="nss" className="form-label">NSS</label>
+              <input
+                type="text"
+                className="form-control"
+                id="nss"
+                placeholder="Ingresa NSS"
+                name="nss"
+                value={paciente.nss}
+                onChange={(e) => inputChange(e)}
+              />
+            </div>
 
-        <div className="mb-2">
-          <label>Domicilio</label>
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Ingresa Domicilio"
-            name='nombre'
-            value={paciente.domicilio} onChange={(e) => inputChange(e)}
-          />
+            <div className="mb-3">
+              <label htmlFor="domicilio" className="form-label">Domicilio</label>
+              <input
+                type="text"
+                className="form-control"
+                id="domicilio"
+                placeholder="Ingresa Domicilio"
+                name="domicilio"
+                value={paciente.domicilio}
+                onChange={(e) => inputChange(e)}
+              />
+            </div>
+          </div>
         </div>
       </form>
     </>
